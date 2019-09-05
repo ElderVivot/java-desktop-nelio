90fa8cc3b956f05c0ab6d112a9e73058c922d0f5
ElderVivot <eldervivot@gmail.com>
2019-09-05 06:20:30 -0300

    Ajustado a funcao loadView pra receber um parametro de inicializacao, no caso o Consumer

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -33,14 +34,19 @@ public class MainViewController implements Initializable {
 		System.out.println("onMenuItemSellerAction");
 	}
 	
+	// o segundo parâmetro é chamada pra carregar os dados que estão no DepartmentList
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController departmentListController) -> {
+			departmentListController.setDepartmentService(new DepartmentService());
+			departmentListController.updateTableView();
+		});
 	}
 	
+	// como esta tela de about não faz nada, então o segundo argumento será uma expressão lâmbda que não faz nada
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
@@ -50,7 +56,8 @@ public class MainViewController implements Initializable {
 	// quando chamos ele vai abrir uma nova tela
 	// o synchoronized serve pra tela não apresentar algum erro inesperado, pois como telas são multithreading pode ocorrer
 	// dela começar a executar e ser interrompida por outras
-	private synchronized void loadView(String absoluteName) {
+	// o Consumer é pra poder chamar telas
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			// instancia uma nova tela
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -75,41 +82,13 @@ public class MainViewController implements Initializable {
 			// adiciona todos os filhos do newVBox no children
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	// temp
-	private synchronized void loadView2(String absoluteName) {
-		try {
-			// instancia uma nova tela
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			
-			// chama cena pra abrir o about dentro da janela principal
-			Scene mainScene = Main.getMainScene();
-			
-			// o getRoot pega a tag ScrollPane, por isto existe um casting pra isto
-			// o getContent pega o conteúdo do ScroolPane, que no caso é um VBox, por isto o casting
-			VBox mainVBox = (VBox)( (ScrollPane) mainScene.getRoot() ).getContent();
-			
-			// getChildren.get(0) pega o MenuBar
-			Node mainMenu = mainVBox.getChildren().get(0);
-			
-			// limpa todos os filhos do mainVBox
-			mainVBox.getChildren().clear();
-			
-			// adiciona somente o MenuBar no children
-			mainVBox.getChildren().add(mainMenu);
-			
-			// adiciona todos os filhos do newVBox no children
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			// temporário, onde estou adicionando a dependência manualmente
-			DepartmentListController departmentController = loader.getController();
-			departmentController.setDepartmentService(new DepartmentService());
-			departmentController.updateTableView();
+			// o T é o do consumer
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			// a versão disto sem o Consumer seria
+			//DepartmentListController departmentController = loader.getController();
+			//departmentController.setDepartmentService(new DepartmentService());
+			//departmentController.updateTableView();
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
